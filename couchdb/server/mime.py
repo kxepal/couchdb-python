c5f3bfa5f2a44f6e47bc8dfdcf35da35408b54d0
Alexander Shorin <kxepal@gmail.com>
2011-08-19 14:52:29 +0400

    update mime module and tests:
    Mime provides functions are grouped to class with own state.

--- couchdb/server/mime.py
+++ couchdb/server/mime.py
@@ -6,12 +6,9 @@ from couchdb.server.exceptions import Error
 log = logging.getLogger(__name__)
 
 __all__ = ['best_match',
-           'mimes_by_key', 'keys_by_mime', 'resp_content_type',
            'provides_used', 'reset_provides',
            'register_type', 'provides', 'run_provides']
 
-################################################################################
-# Mimeparse
 
 def parse_mimetype(mimetype):
     parts = mimetype.split(';')
@@ -61,104 +58,12 @@ def best_match(supported, header):
     return weighted and weighted[-1][0][1] and weighted[-1][2] or ''
 
 
-################################################################################
-# Mime
-
-mimes_by_key = {}
-keys_by_mime = {}
-funcs_by_key = {}
-resp_content_type = None
-
-
-def provides_used():
-    return bool(funcs_by_key)
-
-def reset_provides():
-    global resp_content_type
-    resp_content_type = None
-    funcs_by_key.clear()
-
-def register_type(key, *args):
-    """Register MIME types.
-
-    :param key: Shorthand key for list of MIME types.
-    :type key: str
-
-    :param args: List of full quality names of MIME types.
-
-    Predefined types:
-        - all: ``*/*``
-        - text: ``text/plain; charset=utf-8``, ``txt``
-        - html: ``text/html; charset=utf-8``
-        - xhtml: ``application/xhtml+xml``, ``xhtml``
-        - xml: ``application/xml``, ``text/xml``, ``application/x-xml``
-        - js: ``text/javascript``, ``application/javascript``,
-          ``application/x-javascript``
-        - css: ``text/css``
-        - ics: ``text/calendar``
-        - csv: ``text/csv``
-        - rss: ``application/rss+xml``
-        - atom: ``application/atom+xml``
-        - yaml: ``application/x-yaml``, ``text/yaml``
-        - multipart_form: ``multipart/form-data``
-        - url_encoded_form: ``application/x-www-form-urlencoded``
-        - json: ``application/json``, ``text/x-json``
-
-    Example:
-        >>> register_type('png', 'image/png')
-    """
-    mimes_by_key[key] = args
-    for item in args:
-        keys_by_mime[item] = key
-
-def provides(key, func):
-    """Register MIME type handler which will be called when design function
-    would be requested with matched `Content-Type` value.
-
-    :param key: MIME type.
-    :type key: str
-
-    :param func: Function object or any callable.
-    :type func: function or callable
-    """
-    funcs_by_key[key] = func
-
-def run_provides(req):
-    global resp_content_type
-    bestfun = None
-    bestkey = None
-    accept = None
-    if 'headers' in req:
-        accept = req['headers'].get('Accept')
-    if 'query' in req and 'format' in req['query']:
-        bestkey = req['query']['format']
-        if bestkey in mimes_by_key:
-            resp_content_type = mimes_by_key[bestkey][0]
-    elif accept:
-        supported_mimes = (mime
-           for key in funcs_by_key
-           for mime in mimes_by_key[key]
-           if key in mimes_by_key)
-        resp_content_type = best_match(supported_mimes, accept)
-        bestkey = keys_by_mime.get(resp_content_type)
-    else:
-        bestkey = funcs_by_key and funcs_by_key.keys()[0] or None
-    if bestkey is not None:
-        bestfun = funcs_by_key.get(bestkey)
-    if bestfun is not None:
-        return bestfun()
-    supported_types = ','.join(
-        ', '.join(value) or key for key, value in mimes_by_key.items())
-    raise Error('not_acceptable',
-                'Content-Type %s not supported, try one of:\n'
-                '%s' % (accept or bestkey, supported_types))
-
 # Some default types
 # Ported from Ruby on Rails
 # Build list of Mime types for HTTP responses
 # http://www.iana.org/assignments/media-types/
 # http://dev.rubyonrails.org/svn/rails/trunk/actionpack/lib/action_controller/mime_types.rb
-types = {
+DEFAULT_TYPES = {
     'all': ['*/*'],
     'text': ['text/plain; charset=utf-8', 'txt'],
     'html': ['text/html; charset=utf-8'],
@@ -178,5 +83,96 @@ types = {
     # http://www.ietf.org/rfc/rfc4627.txt
     'json': ['application/json', 'text/x-json']
 }
-for k, v in types.items():
-    register_type(k, *v)
+
+
+class MimeProvider(object):
+
+    def __init__(self):
+        self.mimes_by_key = {}
+        self.keys_by_mime = {}
+        self.funcs_by_key = {}
+        self._resp_content_type = None
+
+        for k, v in DEFAULT_TYPES.items():
+            self.register_type(k, *v)
+
+    def is_provides_used(self):
+        return bool(self.funcs_by_key)
+
+    @property
+    def resp_content_type(self):
+        return self._resp_content_type
+
+    def register_type(self, key, *args):
+        """Register MIME types.
+
+        :param key: Shorthand key for list of MIME types.
+        :type key: str
+
+        :param args: List of full quality names of MIME types.
+
+        Predefined types:
+            - all: ``*/*``
+            - text: ``text/plain; charset=utf-8``, ``txt``
+            - html: ``text/html; charset=utf-8``
+            - xhtml: ``application/xhtml+xml``, ``xhtml``
+            - xml: ``application/xml``, ``text/xml``, ``application/x-xml``
+            - js: ``text/javascript``, ``application/javascript``,
+              ``application/x-javascript``
+            - css: ``text/css``
+            - ics: ``text/calendar``
+            - csv: ``text/csv``
+            - rss: ``application/rss+xml``
+            - atom: ``application/atom+xml``
+            - yaml: ``application/x-yaml``, ``text/yaml``
+            - multipart_form: ``multipart/form-data``
+            - url_encoded_form: ``application/x-www-form-urlencoded``
+            - json: ``application/json``, ``text/x-json``
+
+        Example:
+            >>> register_type('png', 'image/png')
+        """
+        self.mimes_by_key[key] = args
+        for item in args:
+            self.keys_by_mime[item] = key
+
+    def provides(self, key, func):
+        """Register MIME type handler which will be called when design function
+        would be requested with matched `Content-Type` value.
+
+        :param key: MIME type.
+        :type key: str
+
+        :param func: Function object or any callable.
+        :type func: function or callable
+        """
+        self.funcs_by_key[key] = func
+
+    def run_provides(self, req):
+        bestfun = None
+        bestkey = None
+        accept = None
+        if 'headers' in req:
+            accept = req['headers'].get('Accept')
+        if 'query' in req and 'format' in req['query']:
+            bestkey = req['query']['format']
+            if bestkey in self.mimes_by_key:
+                self._resp_content_type = self.mimes_by_key[bestkey][0]
+        elif accept:
+            supported_mimes = (mime
+               for key in self.funcs_by_key
+               for mime in self.mimes_by_key[key]
+               if key in self.mimes_by_key)
+            self._resp_content_type = best_match(supported_mimes, accept)
+            bestkey = self.keys_by_mime.get(self._resp_content_type)
+        else:
+            bestkey = self.funcs_by_key and self.funcs_by_key.keys()[0] or None
+        if bestkey is not None:
+            bestfun = self.funcs_by_key.get(bestkey)
+        if bestfun is not None:
+            return bestfun()
+        supported_types = ','.join(
+            ', '.join(value) or key for key, value in self.mimes_by_key.items())
+        raise Error('not_acceptable',
+                    'Content-Type %s not supported, try one of:\n'
+                    '%s' % (accept or bestkey, supported_types))
--- couchdb/tests/server/mime.py
+++ couchdb/tests/server/mime.py
@@ -8,14 +8,7 @@ from couchdb.server import mime
 class MimeTestCase(unittest.TestCase):
 
     def setUp(self):
-        mime.reset_provides()
-        self._kbm = mime.keys_by_mime.copy()
-        self._mbk = mime.mimes_by_key.copy()
-
-    def tearDown(self):
-        mime.keys_by_mime = self._kbm.copy()
-        mime.mimes_by_key = self._mbk.copy()
-
+        self.provider = mime.MimeProvider()
 
 class MimeToolsTestCase(MimeTestCase):
 
@@ -64,13 +57,13 @@ class MimeToolsTestCase(MimeTestCase):
 
     def test_register_type(self):
         """should register multiple mimetypes for single keyword"""
-        mime.register_type('foo', 'x-foo/bar', 'x-foo/baz')
-        self.assertTrue('foo' in mime.mimes_by_key)
-        self.assertEqual(mime.mimes_by_key['foo'], ('x-foo/bar', 'x-foo/baz'))
-        self.assertTrue('x-foo/bar' in mime.keys_by_mime)
-        self.assertEqual(mime.keys_by_mime['x-foo/bar'], 'foo')
-        self.assertTrue('x-foo/baz' in mime.keys_by_mime)
-        self.assertEqual(mime.keys_by_mime['x-foo/baz'], 'foo')
+        self.provider.register_type('foo', 'x-foo/bar', 'x-foo/baz')
+        self.assertTrue('foo' in self.provider.mimes_by_key)
+        self.assertEqual(self.provider.mimes_by_key['foo'], ('x-foo/bar', 'x-foo/baz'))
+        self.assertTrue('x-foo/bar' in self.provider.keys_by_mime)
+        self.assertEqual(self.provider.keys_by_mime['x-foo/bar'], 'foo')
+        self.assertTrue('x-foo/baz' in self.provider.keys_by_mime)
+        self.assertEqual(self.provider.keys_by_mime['x-foo/baz'], 'foo')
 
 
 class ProvidesTestCase(MimeTestCase):
@@ -81,15 +74,15 @@ class ProvidesTestCase(MimeTestCase):
             return 'foo'
         def bar():
             return 'bar'
-        mime.provides('foo', foo)
-        mime.provides('bar', bar)
-        self.assertEqual(mime.run_provides({}), 'foo')
+        self.provider.provides('foo', foo)
+        self.provider.provides('bar', bar)
+        self.assertEqual(self.provider.run_provides({}), 'foo')
 
     def test_fail_for_unknown_mimetype(self):
         """should raise Error if there is no information about mimetype and
         registered providers"""
         try:
-            mime.run_provides({})
+            self.provider.run_provides({})
         except Exception, err:
             self.assertTrue(isinstance(err, exceptions.Error))
             self.assertEqual(err.args[0], 'not_acceptable')
@@ -98,44 +91,44 @@ class ProvidesTestCase(MimeTestCase):
         """should provides result of registered function for custom mime"""
         def foo():
             return 'foo'
-        mime.provides('foo', foo)
-        mime.register_type('foo', 'x-foo/bar', 'x-foo/baz')
+        self.provider.provides('foo', foo)
+        self.provider.register_type('foo', 'x-foo/bar', 'x-foo/baz')
         self.assertEqual(
-            mime.run_provides({'headers': {'Accept': 'x-foo/bar'}}),
+            self.provider.run_provides({'headers': {'Accept': 'x-foo/bar'}}),
             'foo'
         )
         self.assertEqual(
-            mime.run_provides({'headers': {'Accept': 'x-foo/baz'}}),
+            self.provider.run_provides({'headers': {'Accept': 'x-foo/baz'}}),
             'foo'
         )
 
     def test_provides_registered_mime(self):
         """should provides registered function for base mime by Accept header"""
-        mime.provides('html', lambda: 'html')
+        self.provider.provides('html', lambda: 'html')
         self.assertEqual(
-            mime.run_provides({'headers': {'Accept': 'text/html'}}),
+            self.provider.run_provides({'headers': {'Accept': 'text/html'}}),
             'html'
         )
 
     def test_provides_by_query_format(self):
         """should provides registered function for base mime by query param"""
-        mime.provides('html', lambda: 'html')
+        self.provider.provides('html', lambda: 'html')
         self.assertEqual(
-            mime.run_provides({'query': {'format': 'html'}}),
+            self.provider.run_provides({'query': {'format': 'html'}}),
             'html'
         )
 
     def test_provides_uses(self):
         """should set flag if provides uses."""
-        self.assertFalse(mime.provides_used())
-        mime.provides('html', lambda: 'html')
-        self.assertTrue(mime.provides_used())
+        self.assertFalse(self.provider.is_provides_used())
+        self.provider.provides('html', lambda: 'html')
+        self.assertTrue(self.provider.is_provides_used())
 
     def test_missed_mime_key_from_accept_header(self):
         """should raise Error exception if nothing provides"""
         self.assertRaises(
             exceptions.Error,
-            mime.run_provides,
+            self.provider.run_provides,
             {'headers': {'Accept': 'x-foo/bar'}}
         )
 
@@ -143,14 +136,14 @@ class ProvidesTestCase(MimeTestCase):
         """should raise Error exception if nothing provides"""
         self.assertRaises(
             exceptions.Error,
-            mime.run_provides,
+            self.provider.run_provides,
             {'query': {'format': 'foo'}}
         )
 
     def test_default_mimes(self):
         """should have default registered mimes"""
         self.assertEqual(
-            sorted(mime.mimes_by_key.keys()),
+            sorted(self.provider.mimes_by_key.keys()),
             sorted(['all', 'atom', 'css', 'csv', 'html', 'ics', 'js', 'json',
              'multipart_form', 'rss', 'text', 'url_encoded_form', 'xhtml',
              'xml', 'yaml'])
@@ -160,18 +153,8 @@ class ProvidesTestCase(MimeTestCase):
         """should provides new handler"""
         def foo():
             return 'foo'
-        mime.provides('foo', foo)
-        self.assertTrue('foo' in mime.funcs_by_key)
-
-    def test_reset_provides(self):
-        """should reset all provides"""
-        def foo():
-            return 'foo'
-        self.assertEqual(mime.funcs_by_key, {})
-        mime.provides('foo', foo)
-        self.assertEqual(mime.funcs_by_key, {'foo': foo})
-        mime.reset_provides()
-        self.assertEqual(mime.funcs_by_key, {})
+        self.provider.provides('foo', foo)
+        self.assertTrue('foo' in self.provider.funcs_by_key)
 
 
 def suite():
