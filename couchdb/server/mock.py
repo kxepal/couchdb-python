179a82ac0ebbc5d0d2032b8cc5374295077abd92
Alexander Shorin <kxepal@gmail.com>
2011-08-25 12:50:25 +0400

    allow modify input stream and read output one for MockQueryServer

--- couchdb/server/mock.py
+++ couchdb/server/mock.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 #
+from couchdb import json
 from cStringIO import StringIO
 from collections import deque
 from couchdb.server import SimpleQueryServer
@@ -12,3 +13,13 @@ class MockQueryServer(SimpleQueryServer):
         kwargs.setdefault('input', self._m_input)
         kwargs.setdefault('output', self._m_output)
         super(MockQueryServer, self).__init__(*args, **kwargs)
+
+    def m_input_write(self, data):
+        self._m_input.append(json.encode(data))
+
+    def m_output_read(self):
+        res = []
+        for line in StringIO(self._m_output.getvalue()):
+            res.append(json.decode(line))
+        return res
+
