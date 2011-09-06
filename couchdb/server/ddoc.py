6722d6ba57883ea6f7c0b19d0546465b8d7ee509
Alexander Shorin <kxepal@gmail.com>
2011-09-06 20:48:03 +0400

    refactor DDoc class

--- couchdb/server/ddoc.py
+++ couchdb/server/ddoc.py
@@ -13,7 +13,7 @@ class DDoc(object):
 
     :param commands: Mapping of commands to their callable handlers. Each
                      command actually is the first item in design function path.
-                     See :meth:`process_request` for more information.
+                     See :meth:`run_ddoc_func` for more information.
     :type commands: dict
 
     :param others: Commands defined in keyword style. Have higher priority above
@@ -30,18 +30,18 @@ class DDoc(object):
     def __call__(self, *args, **kwargs):
         return self.process_request(*args, **kwargs)
 
-    def process_request(self, server, *args, **kwargs):
-        """Processes design functions stored within design documents.
+    def process_request(self, server, cmd, *args):
+        """Processes design functions stored within design documents."""
+        if cmd == 'new':
+            return self.add_ddoc(server, *args)
+        else:
+            return self.run_ddoc_func(server, cmd, *args)
 
-        Also holds cache of design documents, but ddoc must have to be
-        registered before proceeding.
 
-        :command: ddoc
-
-        To put ddoc into cache:
-
-        :param new: String constant "new".
-        :type new: unicode
+    def add_ddoc(self, server, ddoc_id, ddoc):
+        """
+        :param server: Query server instance.
+        :type server: :class:`~couchdb.server.BaseQueryServer`
 
         :param ddoc_id: Design document id.
         :type ddoc_id: unicode
@@ -51,58 +51,57 @@ class DDoc(object):
 
         :return: True
 
-        To call function from ddoc:
+        .. versionadded:: 0.11.0
+        """
+        log.info('Cache design document `%s`', ddoc_id)
+        self.cache[ddoc_id] = ddoc
+        return True
+
+    def run_ddoc_func(self, server, ddoc_id, fun_path, fun_args):
+        """
+        :param server: Query server instance.
+        :type server: :class:`~couchdb.server.BaseQueryServer`
 
         :param ddoc_id: Design document id, holder of requested function.
         :type ddoc_id: unicode
 
-        :param func_path: List of key by which request function could be found
-                          within ddoc object. First element of this list is
-                          ddoc command.
-        :type func_path: list
+        :param fun_path: List of key by which request function could be found
+                         within ddoc object. First element of this list is
+                         ddoc command.
+        :type fun_path: list
 
-        :param func_args: List of function arguments.
-        :type func_args: list
+        :param fun_args: List of function arguments.
+        :type fun_args: list
 
-        :return: If ddoc putted into cache True will be returned.
-            If ddoc function called returns it's result if any exists.
-            For example, lists doesn't explicitly returns any value.
+        :return: Result of called design function if any available.
+                 For example, lists doesn't explicitly returns any value.
 
         .. versionadded:: 0.11.0
         """
-        args = list(args)
-        ddoc_id = args.pop(0)
-        if ddoc_id == 'new':
-            ddoc_id = args.pop(0)
-            self.cache[ddoc_id] = args.pop(0)
-            log.debug('Added `%s` design document to cache', ddoc_id)
-            return True
+        ddoc = self.cache.get(ddoc_id)
+        if ddoc is None:
+            msg = 'Uncached design document: %s' % ddoc_id
+            log.error(msg)
+            raise FatalError('query_protocol_error', msg)
+        cmd = fun_path[0]
+        if cmd not in self.commands:
+            msg = 'Unknown ddoc command `%s`' % cmd
+            log.error(msg)
+            raise FatalError('unknown_command', msg)
+        handler = self.commands[cmd]
+        point = ddoc
+        for item in fun_path:
+            prev, point = point, point.get(item)
+            if point is None:
+                msg = 'Missed function `%s` in design doc `%s` by path: %s'
+                args = (item, ddoc_id, '/'.join(fun_path))
+                log.error(msg, args)
+                raise Error('not_found', msg % args)
         else:
-            ddoc = self.cache.get(ddoc_id)
-            log.debug('Initiate work with `%s` design document', ddoc_id)
-            if ddoc is None:
-                log.error('Uncached design doc `%s`' % ddoc_id)
-                raise FatalError('query_protocol_error',
-                                 'uncached design doc: %s' % ddoc_id)
-            fun_path = args.pop(0)
-            cmd = fun_path[0]
-            func_args = args.pop(0)
-            log.debug('Processing DDoc command `%s`', cmd)
-            if cmd not in self.commands:
-                log.error('Unknown ddoc command `%s`' % cmd)
-                raise FatalError('unknown_command',
-                                 'unknown ddoc command `%s`' % cmd)
-            handler = self.commands[cmd]
-            point = ddoc
-            for item in fun_path:
-                prev, point = point, point.get(item)
-                if point is None:
-                    msg = 'Missing %s function %s on design doc %s'
-                    log.error(msg, cmd, item, ddoc_id)
-                    raise Error('not_found', msg % (cmd, item, ddoc_id))
-            else:
-                func = point
-                if type(func) is not FunctionType:
-                    func = server.compile(func, ddoc)
-                    prev[item] = func
-            return handler(server, func, *func_args)
+            func = point
+            if type(func) is not FunctionType:
+                func = server.compile(func, ddoc)
+                prev[item] = func
+        log.info('Run function `%s` in design doc `%s` by path: %s',
+                 func.__name__, ddoc_id, '/'.join(fun_path))
+        return handler(server, func, *fun_args)
