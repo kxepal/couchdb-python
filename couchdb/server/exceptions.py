e070044d2f59b44f497ab927ed172a74a8584d1b
Alexander Shorin <kxepal@gmail.com>
2011-09-07 22:14:05 +0400

    fix exceptions docstrings

--- couchdb/server/exceptions.py
+++ couchdb/server/exceptions.py
@@ -2,13 +2,13 @@
 #
 
 class ViewServerException(Exception):
-    '''Base query server exception'''
+    """Base query server exception"""
 
 class Error(ViewServerException):
-    '''Non fatal error which doesn't initiate query server termination.'''
+    """Non fatal error which should not terminate query serve"""
 
 class FatalError(ViewServerException):
-    '''Fatal error which terminates query server.'''
+    """Fatal error which should terminates query server"""
 
 class Forbidden(ViewServerException):
-    '''Non fatal error which signs operation access deny.'''
+    """Non fatal error which signs access deny for processed operation"""
