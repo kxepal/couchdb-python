bd96fde547d40e27f730634abbab19eb36f55289
Alexander Shorin <kxepal@gmail.com>
2011-09-08 12:38:06 +0400

    refactor validate module:
    - group exception handlers
    - add debug logging
    - add server information to docstrings

--- couchdb/server/validate.py
+++ couchdb/server/validate.py
@@ -7,36 +7,41 @@ __all__ = ['validate', 'ddoc_validate']
 
 log = logging.getLogger(__name__)
 
-def handle_error(err, userctx):
+def handle_error(func, err, userctx):
     if isinstance(err, Forbidden):
         reason = err.args[0]
-        log.warn('Access deny for user %s. Reason: %s', userctx, reason)
+        log.warn('Access deny: %s\nuserctx: %s\nfunc: %s',
+                 reason, userctx, func)
         raise
     elif isinstance(err, AssertionError):
         # This is custom behavior that allows to use assert statement
         # for field validation. It's just quite handy.
-        log.warn('Access deny for user %s. Reason: %s', userctx, err)
+        log.warn('Access deny: %s\nuserctx: %s\nfunc: %s',
+                 err, userctx, func)
         raise Forbidden(str(err))
+    elif isinstance(err, ViewServerException):
+        log.exception('%s exception raised by %s',
+                      err.__class__.__name__, func)
+        raise
+    else:
+        log.exception('Something went wrong in %s', func)
+        raise Error(err.__class__.__name__, str(err))
 
 def run_validate(func, *args):
+    log.debug('Run %s for userctx:\n%s', func, args[2])
     try:
         func(*args)
-    except (AssertionError, Forbidden), err:
-        handle_error(err, args[2])
-    except ViewServerException, err:
-        log.exception('%s exception raised by %s validate_doc_update function'
-                      '' % (err.__class__.__name__, func.__name__))
-        raise
     except Exception, err:
-        log.exception('Something went wrong at %s validate_doc_update function'
-                      '' % func.__name__)
-        raise Error(err.__class__.__name__, str(err))
+        handle_error(func, err, args[2])
     return 1
 
 def validate(server, funsrc, newdoc, olddoc, userctx):
     """Implementation of `validate` command.
 
     :command: validate
+
+    :param server: Query server instance.
+    :type server: :class:`~couchdb.server.BaseQueryServer`
 
     :param funsrc: validate_doc_update function source.
     :type funsrc: unicode
@@ -65,6 +70,9 @@ def ddoc_validate(server, func, newdoc, olddoc, userctx, secobj=None):
 
     :command: validate_doc_update
 
+    :param server: Query server instance.
+    :type server: :class:`~couchdb.server.BaseQueryServer`
+
     :param func: validate_doc_update function.
     :type func: function
 
@@ -91,7 +99,8 @@ def ddoc_validate(server, func, newdoc, olddoc, userctx, secobj=None):
         if func.func_code.co_argcount == 3:
             log.warning('Since 0.11.1 CouchDB validate_doc_update functions'
                         ' takes additional 4th argument `secobj`.'
-                        ' Please, update your code to remove this warning.')
+                        ' Please, update your code for %s to remove'
+                        ' this warning.', func)
             args = args[:3]
     else:
         args = args[:3]
