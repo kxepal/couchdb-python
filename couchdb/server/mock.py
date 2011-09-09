e3bcd784f4ab64a862f1c6e9da8001fe57415db8
Alexander Shorin <kxepal@gmail.com>
2011-09-09 18:04:22 +0400

    organize imports and make m_input and m_output similar objects

--- couchdb/server/mock.py
+++ couchdb/server/mock.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 #
-from couchdb import json
-from cStringIO import StringIO
 from collections import deque
+from couchdb import json
 from couchdb.server import SimpleQueryServer
 
-class MockStdin(deque):
+class MockStream(deque):
 
     def readline(self):
         if self:
@@ -13,11 +12,20 @@ class MockStdin(deque):
         else:
             return ''
 
+    def write(self, data):
+        if isinstance(data, basestring):
+            self.append(json.decode(data))
+        else:
+            self.append(data)
+
+    def flush(self):
+        pass
+
 class MockQueryServer(SimpleQueryServer):
     """Mock version of Python query server."""
     def __init__(self, *args, **kwargs):
-        self._m_input = MockStdin()
-        self._m_output = StringIO()
+        self._m_input = MockStream()
+        self._m_output = MockStream()
         kwargs.setdefault('input', self._m_input)
         kwargs.setdefault('output', self._m_output)
         super(MockQueryServer, self).__init__(*args, **kwargs)
@@ -26,8 +34,5 @@ class MockQueryServer(SimpleQueryServer):
         self._m_input.append(json.encode(data))
 
     def m_output_read(self):
-        res = []
-        for line in StringIO(self._m_output.getvalue()):
-            res.append(json.decode(line))
-        return res
-
+        output = self._m_output
+        return [output.popleft() for i in range(len(output)) if output]
